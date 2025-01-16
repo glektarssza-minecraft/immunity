07426c466c609537e643aa7716de82644f386300
G'lek Tarssza <glek@glektarssza.com>
2025-01-16 16:45:07 +0000

    Fixed up dev container install script for pre-commit.

--- .devcontainer/features/pre-commit/install.sh
+++ .devcontainer/features/pre-commit/install.sh
@@ -4,8 +4,18 @@
 . ./functions.sh;
 
 # Set up Python 3 venv.
-su ${_REMOTE_USER} -c 'python3 -m venv ~/.python3-venv' && \
-# Add Python 3 venv to path.
-su ${_REMOTE_USER} -c "sed -i 's|export PATH=\"\(.*\)\"|export PATH=\"\$HOME/.python3-venv/bin/:\1\"|' ~/.zshrc ~/.bashrc" && \
+su ${_REMOTE_USER} -c "python3 -m venv ~${_REMOTE_USER}/.python3-venv" && \
+# Disable Python 3 venv prompt in .zshrc.
+su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.zshrc ]; then echo 'VIRTUAL_ENV_DISABLE_PROMPT=\"true\"' >> ~${_REMOTE_USER}/.zshrc; fi"  && \
+# Add Python 3 venv to .zshrc.
+su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.zshrc ]; then echo \"source \"\\\$HOME/.python3-venv/bin/activate\"\" >> ~${_REMOTE_USER}/.zshrc; fi"  && \
+# Disable Python 3 venv prompt in .bashrc.
+su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.bashrc ]; then echo 'VIRTUAL_ENV_DISABLE_PROMPT=\"true\"' >> ~${_REMOTE_USER}/.bashrc; fi"  && \
+# Add Python 3 venv to .bashrc.
+su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.bashrc ]; then echo 'VIRTUAL_ENV_DISABLE_PROMPT=\"true\"' >> ~${_REMOTE_USER}/.bashrc; fi"  && \
+# Disable Python 3 venv prompt in .profile no .bashrc/.zshrc exists.
+su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.profile -a ! -f ~${_REMOTE_USER}/.zshrc -a ! -f ~${_REMOTE_USER}/.bashrc ]; then echo 'VIRTUAL_ENV_DISABLE_PROMPT=\"true\"' >> ~${_REMOTE_USER}/.profile; fi"  && \
+# Add Python 3 venv to .profile if no .bashrc/.zshrc exists.
+su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.profile -a ! -f ~${_REMOTE_USER}/.zshrc -a ! -f ~${_REMOTE_USER}/.bashrc ]; then echo 'VIRTUAL_ENV_DISABLE_PROMPT=\"true\"' >> ~${_REMOTE_USER}/.profile; fi"  && \
 # Install pre-commit.
-su ${_REMOTE_USER} -c 'python3 -m pip install pre-commit';
+su ${_REMOTE_USER} -c "~${_REMOTE_USER}/.python3-venv/bin/pip3 install pre-commit";
