0ee31882180a9329a2d05422049df8aa1d88ab9e
G'lek Tarssza <glek@glektarssza.com>
2025-01-16 18:31:45 +0000

    Added a way to install `gradle`.

---
+++ .devcontainer/features/gradle/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# Import our function library.
+. ./functions.sh;
+
+# Ensure Gradle install location exists.
+mkdir -p /opt/gradle/ && \
+# Ensure Gradle install location is empty.
+rm -rf /opt/gradle/* && \
+# Download version of Gradle to install.
+wget https://services.gradle.org/versions/current -O - | jq --raw-output .downloadUrl | xargs wget --directory-prefix=/opt/gradle/ && \
+# Unpack version of Gradle to install.
+wget https://services.gradle.org/versions/current -O - | jq --raw-output .version | xargs -I V 7z x -o/opt/gradle/ gradle-V-all.zip && \
+# Remove downloaded Gradle zip.
+wget https://services.gradle.org/versions/current -O - | jq --raw-output .version | xargs -I V rm gradle-V-all.zip && \
+# Move unpacked files to main install location.
+wget https://services.gradle.org/versions/current -O - | jq --raw-output .version | xargs -I V mv /opt/gradle/gradle-V/* /opt/gradle/ && \
+# Add Gradle path to .zshrc.
+su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.zshrc ]; then echo 'export PATH=\"/opt/gradle/bin:\\\$PATH\"' >> ~${_REMOTE_USER}/.zshrc; fi"  && \
+# Add Gradle path to .bashrc.
+su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.bashrc ]; then echo 'export PATH=\"/opt/gradle/bin:\\\$PATH\"' >> ~${_REMOTE_USER}/.bashrc; fi"  && \;
+# Add Gradle path to .profile if no .bashrc/.zshrc exists.
+su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.profile -a ! -f ~${_REMOTE_USER}/.zshrc -a ! -f ~${_REMOTE_USER}/.bashrc ]; then echo 'export PATH=\"/opt/gradle/bin:\\\$PATH\"' >> ~${_REMOTE_USER}/.profile; fi";
